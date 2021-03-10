533408a5b5161515b6efbb9964992f4be9a01c10
Abdul rehman Shaukat <abdulrehman.shoukat@email.com>
2021-03-10 18:17:14 +0000

    1

---
+++ client.js
@@ -0,0 +1,14 @@
+const net = require('net');
+
+const connect = function() {
+  const conn = net.createConnection({ 
+    host: '135.23.222.131',
+    port: 50542
+  });
+  // interpret incoming data as text
+  conn.setEncoding('utf8'); 
+  console.log('connected')
+  return conn;
+}
+
+module.exports = connect;
--- play.js
+++ play.js
@@ -1,18 +1,7 @@
-const net = require('net');
+const connect = require("./client");
 
-const connect = function() {
-  const conn = net.createConnection({ 
-    host: '135.23.222.131',
-    port: 50542
-  });
-  // interpret incoming data as text
-  conn.setEncoding('utf8'); 
-  console.log('connected')
-  return conn;
-}
 
-console.log('Connecting ...');
-connect();
+//connect();
 const client = connect()
 
 client.on("data", (data) => {
