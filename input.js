1d419201a13596b2337312d18d08127c626549c3
Abdul rehman Shaukat <abdulrehman.shoukat@email.com>
2021-03-11 02:46:30 +0000

    move

--- input.js
+++ input.js
@@ -5,20 +5,23 @@ const setupInput = function(conn) {
     stdin.setRawMode(true);
     stdin.setEncoding('utf8');
     stdin.resume();
-    stdin.on('data',key => handleUserInput(key))
+    stdin.on('data',(key) => handleUserInput(key))
     return stdin;
   }
   
   const handleUserInput = (key)=> {
     if (key === '\u0003') {
-      console.log('hello')
+      console.log('Exiting')
       process.exit();
+    } else if (key === 'w') {
+      connection.write( "Move: up" );
+    } else if (key === 's') {
+      connection.write( "Move: down" );
+    } else if (key === 'a') {
+      connection.write( "Move: left" );
+    } else if (key === 'd') {
+      connection.write( "Move: right" );
     } 
-    // else {
-    //   stdin.on('data', (key) => {
-    //     process.stdout.write('.');
-    //   });
-    //}
   }
 
-  module.exports = setupInput;
+module.exports = setupInput;
